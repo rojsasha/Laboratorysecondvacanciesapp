4f5061af6dec3e8e56b880dd05de4762ae4abe47
rojsasha <rojsasha@gmail.com>
2018-04-29 03:36:34 +0600

    create saving, delete and show favorite vacancies(lost 1 file)

---
+++ app/src/main/java/com/example/rojsa/laboratorysecondvacanciesapp/ui/FavoriteVacancyActivity.java
@@ -0,0 +1,32 @@
+package com.example.rojsa.laboratorysecondvacanciesapp.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.ListView;
+
+import com.example.rojsa.laboratorysecondvacanciesapp.R;
+import com.example.rojsa.laboratorysecondvacanciesapp.StartApplication;
+import com.example.rojsa.laboratorysecondvacanciesapp.data.SQLiteHelper;
+import com.example.rojsa.laboratorysecondvacanciesapp.model.AllDayModel;
+
+import java.util.List;
+
+public class FavoriteVacancyActivity extends BaseActivity {
+    SQLiteHelper sqLiteHelper;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_vacancies_over_day);
+        sqLiteHelper = StartApplication.get(getApplicationContext()).getSqLiteHelper();
+        getFavoriteData();
+    }
+
+
+    private void getFavoriteData(){
+        ListView listView = findViewById(R.id.recycleView);
+        List<AllDayModel> list = sqLiteHelper.getFavoriteVacancy();
+        if (list.isEmpty())return;
+        RecycleViewAdapter adapter = new RecycleViewAdapter(getApplicationContext(),list);
+        listView.setAdapter(adapter);
+    }
+}
